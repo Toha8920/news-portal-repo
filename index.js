d3251f95b04b5a17dc2b8c3c4aa5aa61ea858782
Toha Islam <islamtoha710@gmail.com>
2022-09-03 01:24:15 +0600

    clicked card done

--- index.js
+++ index.js
@@ -34,9 +34,29 @@ const loadCategoryDetails = async (category_id) => {
 
 const displayCategoryDetails = async (categorys) => {
     console.log(categorys)
-    const sectionDetails = document.getElementById('section-details')
+    const sectionDetails = document.getElementById('section-details');
+    sectionDetails.textContent = ``
     categorys.forEach(category => {
         console.log(category)
+        const sectionDiv = document.createElement('div')
+        sectionDiv.classList.add('card')
+        sectionDiv.classList.add('mb-3')
+        sectionDiv.innerHTML = `
+        <div class="row g-0 m-3">
+        <div class="col-md-4">
+            <img id="img-feild" src="${category.thumbnail_url}" class="img-fluid rounded-start" alt="...">
+        </div>
+        <div class="col-md-8">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural
+                    lead-in to additional content. This content is a little bit longer.</p>
+                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+        </div>
+    </div>
+       `;
+        sectionDetails.appendChild(sectionDiv)
     })
 
 }
